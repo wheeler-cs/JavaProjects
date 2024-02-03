261317f1f1fc65f3da30ed18a5e9d48ff4e1cf94
wheeler-cs <wheelercs@protonmail.com>
2024-02-03 15:50:06 -0600

    Added AI's Move For Very First Turn
    AI will always go for the center tile if it's the first turn of the
    game.
    
    Signed-off-by: wheeler-cs <wheelercs@protonmail.com>

--- 01_TicTacToe/AI.java
+++ 01_TicTacToe/AI.java
@@ -1,7 +1,35 @@
+import java.io.IOException;
+
 public class AI extends Player {
+
+    int nextMove = 0;
+
+    @Override
+    public int chooseCell() throws IOException {
+        return this.nextMove;
+    }
 
     @Override
     public void intakeBoard(int[] gameVector) {
-        // TODO: Implement algorithm for AI (minimax)
+        // Check to see if board is empty
+        boolean isFirstTurn = true;
+        for(int i = 0; i < 9; i++) {
+            if(gameVector[i] != 0) {
+                isFirstTurn = false;
+                break;
+            }
+        }
+        // Monopolize on the center cell if it's the first turn (don't need algo for that)
+        if(isFirstTurn) {
+            this.nextMove = 4;
+        }
+        // Need a more complex algorithm because moves have already been made
+        else {
+            this.nextMove = this.calculateNextMove(gameVector);
+        }
+    }
+
+    public int calculateNextMove(int[] gameVectory) {
+        return 0;
     }
 }
